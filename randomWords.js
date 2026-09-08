664fab6634ec64359f8c14a45079c851442bdd08
DragosAndries302 <dragosionutandries@students.brillantmont.ch>
2026-09-08 14:05:38 +0200

    Fix syntax error in randomWords.js

--- randomWords.js
+++ randomWords.js
@@ -3,4 +3,4 @@ function generateRandomWord() {
   const randomIndex = Math.floor(Math.random() * words.length);
   const randomWord = words[randomIndex]
   document.getElementById("wordDisplay").innerText = randomWord;
-]
+}
